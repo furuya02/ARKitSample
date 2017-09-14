90e2dbe2772e91574dd913ea5e0a0d5dd68666bb
HIRAUCHI <hirauchi.shinichi@classmethod.jp>
2017-09-14 15:36:33 +0900

    GM版への対応

--- ARKitSample/ViewController.swift
+++ ARKitSample/ViewController.swift
@@ -18,7 +18,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.viewDidLoad()
         
         sceneView.delegate = self
-        
         sceneView.scene = SCNScene() // ビューのsceneプロパティに空のシーンを生成する
         
         let node = SCNNode() // ノードを生成
@@ -29,63 +28,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         node.position = SCNVector3(0, 0, -0.5) // ノードの位置は、カメラを原点として左右：0m 上下：0m　奥に50cm
         
         sceneView.scene.rootNode.addChildNode(node) // 生成したノードをシーンに追加する
-        
-//        sceneView.scene = SCNScene()
-//        let node = SCNNode()
-//        node.geometry = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
-//        let material = SCNMaterial()
-//        material.diffuse.contents = UIImage(named: "brick")
-//        node.geometry?.materials = [material]
-//        node.position = SCNVector3(0, 0, -0.5)
-//
-//        sceneView.scene.rootNode.addChildNode(node)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-        
-        // Run the view's session
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        // Pause the view's session
         sceneView.session.pause()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
